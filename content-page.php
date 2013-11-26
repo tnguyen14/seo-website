02880323c351945816fd6845ebbbedf704bb634f
Tri Nguyen <tri@tridnguyen.com>
2013-11-25 22:41:11 -0500

    template for regular page

--- content-page.php
+++ content-page.php
@@ -8,24 +8,13 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h1 class="entry-title"><?php the_title(); ?></h1>
+		<h1 class="page-title"><?php the_title();?></h1>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php the_content();   
-    ?>
-    <div id="meta">
-      <?php
-          the_meta();
-      ?>
-    </div>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'seo-vietnam' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_content();?>
+		<div id="meta">
+			<?php the_meta(); ?>
+		</div>
 	</div><!-- .entry-content -->
-	<?php edit_post_link( __( 'Edit', 'seo-vietnam' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 </article><!-- #post-## -->
